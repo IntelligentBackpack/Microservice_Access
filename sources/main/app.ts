7b2dab7080273f266a03e9036396ff8ddce5fe20
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2023-04-25 14:01:25 +0200

    fix: added bodyparser for json

--- sources/main/app.ts
+++ sources/main/app.ts
@@ -1,9 +1,11 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+const bodyParser = require('body-parser');
 
 dotenv.config();
 
 const app: Express = express();
+app.use(bodyParser.json());
 
 app.get('/', async (req: Request, res: Response) => {
   	res.send('Access control microservice');
